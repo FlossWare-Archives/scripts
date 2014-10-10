9d2cfabc4908fbcd514ebe0713d057f748b9c48b
Scot P. Floess <sfloess@redhat.com>
2014-10-10 11:11:20 -0400

    Issue #5 - can now parse out version from pom.xml files

---
+++ scripts/get-pom-version.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ensureParam() {
+    if [ $# -lt 1 -o $# -gt 1 ]
+    then
+        echo
+        echo "ERROR"
+        echo "  Please provide a lone parameter that denotes the"
+        echo "  maven pom file!"
+        echo
+        echo "Example:"
+        echo "  `dirname $0`/$0 /var/lib/openshift/540b2c664382ec0a4f000622/app-root/data/workspace/FlossWare/pom.xml"
+        echo
+        exit 1
+    fi
+}
+
+ensureFileExists() {
+    if [ ! -f $1 ]
+    then
+        echo
+        echo "ERROR"
+        echo "  The file [$1] does not exist!"
+        echo
+        exit 1
+    fi
+}
+
+ensureParam $@
+ensureFileExists $@
+
+echo 'xpath /* [local-name()="project"]/* [local-name()="version"]/text()' | xmllint --shell $1 | grep content | cut -f 2 -d '='
