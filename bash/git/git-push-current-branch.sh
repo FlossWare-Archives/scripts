c546e6b0fd7d97f05d8e7dfe373741f2c1c3e358
Scot P. Floess <sfloess@redhat.com>
2018-01-14 23:57:32 -0500

    NA - needed a convenience script to push out the current git branch

---
+++ bash/git/git-push-current-branch.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#
+# This file is part of the FlossWare family of open source software.
+#
+# FlossWare is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+#
+
+#
+# Open Shift does not allow us to affect change to
+# ~/.ssh as this directory is owned by root.
+#
+# We will use a different directory for ssh identities
+# and must denote this using the ssh-git.sh script.
+#
+# To use:
+#   openshift-git-push.sh [remote] [branch]
+#
+
+. `dirname ${BASH_SOURCE[0]}`/openshift-config.sh
+
+`dirname ${BASH_SOURCE[0]}`/../ssh-git.sh ${OPEN_SHIFT_SSH_DIR}/id_rsa $@
